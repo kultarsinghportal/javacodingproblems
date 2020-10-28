b4a7243e94d578c6b82dfd25ec1952e737f1180d
lenovo <lenovo@192.168.1.4>
2020-10-28 22:43:48 +0530

    1- CountDuplicateCharacters

---
+++ src/main/java/testproject/javacodingproblems/StringCountDuplicateCharacters.java
@@ -0,0 +1,38 @@
+package testproject.javacodingproblems;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.Collectors;
+
+public class StringCountDuplicateCharacters {
+
+	public static void main(String... args) {
+		System.out.println("Kultar Singh");
+		String str = "This is a current and duplicate current string";
+
+		long startTime = System.currentTimeMillis();
+		java.util.concurrent.ConcurrentHashMap<Character, Integer> dupStr = new ConcurrentHashMap<>();
+
+		for (char ch : str.toCharArray()) {
+			dupStr.compute(ch, (k, v) -> (v == null) ? 1 : ++v);
+		}
+		System.out.println(dupStr);
+
+		long endTime = System.currentTimeMillis();
+		System.out.println("First Approach : " + (endTime - startTime));
+
+		long startTime1 = System.currentTimeMillis();
+		System.out.println(countDuplicateCharacters(str));
+		long endTime1 = System.currentTimeMillis();
+		System.out.println("Second Approach : " + (endTime1 - startTime1));
+	}
+
+	public static Map<Character, Long> countDuplicateCharacters(String str) {
+
+		Map<Character, Long> result = str.chars().mapToObj(c -> (char) c)
+				.collect(Collectors.groupingBy(c -> c, Collectors.counting()));
+
+		return result;
+	}
+
+}
